8912d20ff0952d16f0e701d112c5a2d0e28cac79
Grégoire Seux <g.seux@criteo.com>
2019-03-25 16:23:17 +0100

    Remove undefined clang-format make rule references
    This target does not exist. With current code, we have to commit every
    changes before running tests (otherwise it fails with unrelated error)
    
    Change-Id: Iff2d01d8cd71ad5b587ea02d882ce11aa99b2917

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -11,15 +11,6 @@ cd build
 cmake ..
 
 export PATH=$PATH:/opt/llvm-3.8.0/bin
-make clang-format
-
-updated_files_count=`git diff --name-only | wc -l`
-
-if [ "$updated_files_count" -ne "0" ];
-then
-  echo "[FAILED] Please run 'make clang-format' and commit the changes."
-  exit 1
-fi
 
 make
 make check
